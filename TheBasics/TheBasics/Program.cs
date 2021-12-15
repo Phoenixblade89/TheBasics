c8e4469c0999e97739d378dec2235312cfd26857
Jon Essary <jessary.lionheart89@gmail.com>
2021-12-14 21:54:34 -0500

    initial commit

---
+++ TheBasics/TheBasics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TheBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iAmANumber = 1;
+            float iAmANumberWithADecimal = 1.0f;
+            double iAmANumberWithADecimalThatIsReallyBig = 1.000000000;
+            char iAmALetter = 'a';
+            string iAmABunchOfLetters = "Hello Smiley :) Wieners and Wine";
+
+            Console.WriteLine(iAmABunchOfLetters);
+        }
+    }
+}
---
+++ TheBasicsNetFramework/TheBasicsNetFramework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBasicsNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            float f = 0.0f;
+            double d = 0.0;
+            string x = "haha ghey";
+
+            Console.WriteLine(x);
+
+            x = Console.ReadLine();
+
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+    }
+}
